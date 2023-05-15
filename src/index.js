0a8c314ea32dbbfa5c35de891befad0d98382668
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-05-15 12:03:18 +0200

    11

--- src/index.js
+++ src/index.js
@@ -1,93 +1,141 @@
-import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
-let debounce = require('lodash.debounce');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { FetchImg } from './js/fetchImg';
+import { LoadMoreBtn } from './js/loadMoreBtn';
 
-const DEBOUNCE_DELAY = 300;
-const inputEl = document.getElementById('search-box');
-const countryListEl = document.querySelector('.country-list');
-const countryInfoEl = document.querySelector('.country-info');
+const formtEl = document.querySelector('.search__form');
+const galleryEl = document.querySelector('.gallery');
 
-inputEl.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
-//inputEl.addEventListener('input', searchCountry);
+const fetchImg = new FetchImg();
+const showMoreBtnEl = new LoadMoreBtn({
+  selector: '#load-more',
+  isHidden: true,
+});
 
-function searchCountry() {
-  const countries = inputEl.value.trim();
-  console.log(inputEl.value);
-  if (countries == '') {
-    dataOutputInfo('');
-    dataOutputLi('');
-    return;
+formtEl.addEventListener('submit', onSubmit);
+showMoreBtnEl.button.addEventListener('click', onMoreSearch);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const form = evt.currentTarget;
+  fetchImg.search = form.elements.searchQuery.value.trim();
+
+  if (fetchImg.search === '') {
+    clearImgList();
+    showMoreBtnEl.hide();
+    return Notify.failure('Please enter your request');
   }
+  showMoreBtnEl.hide();
+  clearImgList();
+  fetchImg.resetPage();
 
-  fetchCountries(countries)
-    .then(countries => {
-      renderResult(countries);
-    })
-    .catch(`blad pobierania danych`);
+  fetchImgCard().finally(form.reset());
 }
 
-function renderResult(countries) {
-  if (countries.length > 10) {
-    Notify.info('Too many matches found. Please enter a more specific name.');
-  }
-  if (countries.length < 10 && countries.length > 2) {
-    dataOutputInfo('');
-    let markupList = countries
-      .map(country => createListCoutry(country))
-      .join('');
+function onMoreSearch() {
+  showMoreBtnEl.disable();
+  fetchImgCard().then(markup => {
+    scroll();
 
-    return dataOutputLi(markupList);
-  }
-  if (countries.length == 1) {
-    dataOutputLi('');
+    return markup;
+  });
+}
 
-    const markupInfo = countries.map(country => createMurkup(country)).join('');
-    return dataOutputInfo(markupInfo);
+async function fetchImgCard() {
+  try {
+    const data = await fetchImg.getImg();
+    if (!data.totalHits) {
+      showMoreBtnEl.hide();
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    if (!(fetchImg.queryPage > 2)) {
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+
+    let currentPage = (+fetchImg.queryPage - 1) * fetchImg.per_page;
+    console.log(currentPage);
+    let totalImg = data.totalHits;
+    console.log(totalImg);
+
+    if (totalImg <= currentPage) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      showMoreBtnEl.hide();
+    } else {
+      showMoreBtnEl.show();
+    }
+
+    const hits = await data.hits;
+    const listImg = await createMurkup(hits);
+    const murkupList = await updateImgList(listImg);
+    showMoreBtnEl.enable();
+    lightbox.refresh();
+
+    return murkupList;
+  } catch (error) {
+    onError();
+    console.log(error.message);
   }
 }
 
-function createListCoutry(country) {
-  const {
-    name: { common },
-    flags: { svg },
-  } = country;
-  return `
-  <li class="coutry-item" style="display: flex; flex-direction: row; align-items:center; ">
-  <img style="margin-right: 10px; width:50px; height: 30px; border: 1px solid #ccc;"  src="${svg}" alt="flag" class="coutry-flag" >
-  <h2 class="coutry-name">${common}</h2>
-  </li>`;
-}
-
-function createMurkup(country) {
-  const {
-    name: { official },
-    flags: { svg },
-    capital,
-    population,
-    languages,
-  } = country;
-  return `
-  <div style="display:flex; flex-direction:row; align-items: center;">
-  <img src="${svg}" alt="flag" style="width:50px; height: 50px; margin-right: 10px; " class="coutry-flag" >
-  <h1 class="coutry-name">${official}</h1>
+function createMurkup(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+     
+    <div class="photo-card">
+     <a class="photo__item" href="${largeImageURL}">
+  <img class="img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item"><b>Likes</b> ${likes}</p>
+    <p class="info-item"><b>Views</b> ${views}</p>
+    <p class="info-item"><b>Comments</b> ${comments}</p>
+    <p class="info-item"><b>Downloads</b> ${downloads}</p>
   </div>
-  <p class="coutry-text"><b>Capital:</b> ${capital}</p>
-  <p class="coutry-text"><b>Population:</b> ${population}</p>
-  <p class="coutry-text"><b>Languages:</b> ${Object.values(languages).join(
-    ', '
-  )}</p>`;
+</div>`
+    )
+    .join('');
+  return markup;
 }
 
-function dataOutputLi (markup) {
-  countryListEl.innerHTML = markup;
+function updateImgList(markup) {
+  galleryEl.insertAdjacentHTML('beforeend', markup);
 }
-function dataOutputInfo (markup) {
-  countryInfoEl.innerHTML = markup;
-};
 
-function onError () {
-  dataOutputInfo('');
-  dataOutputLi('');
-  Notify.failure('Oops, there is no country with that name');
-};
+function clearImgList() {
+  galleryEl.innerHTML = '';
+}
+
+function onError() {
+  clearImgList();
+  Notify.failure('Please enter your details');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+
+function scroll() {
+  const { height: cardHeight = fetchImg.per_page } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight,
+    behavior: 'smooth',
+  });
+}
